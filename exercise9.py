676a280833bf8994807a2e11fb66174145077217
Sanele <dumisilem@aol.com>
2021-02-05 13:29:52 +0000

    exercise9 part a done, new file created alled operations

---
+++ exercise9.py
@@ -0,0 +1,7 @@
+first_name= "Sanele"
+last_name="Mbuthuma"
+print(first_name, last_name)
+new_list= ["Sanele", "Mbuthuma"]
+print(new_list [0])
+mydict={"name":"sanele", "last":"mbuthuma"}
+print(mydict["name"])
